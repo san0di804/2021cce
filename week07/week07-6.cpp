9a5082eb8c1cc4a56b8b4a132f291df9106a1041
san0di804 <91170798+san0di804@users.noreply.github.com>
2021-10-27 12:03:13 +0800

    Add files via upload

---
+++ week07/week07-6.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{   ///最簡單的基礎型 電腦
+    for(int i=0;i<4;i++){
+        printf("i:%d\n",i);
+    }
+    ///最簡單的基礎型 人類數數字
+    for(int i=1;i<=4;i++){
+        printf("人類的數法i:%d\n",i);
+    }
+    for(int i=0;i<=4;i++){
+        printf("怪怪的...%d\n",i);
+    }
+}
