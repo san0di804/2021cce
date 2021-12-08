8a9df6b9b8263cca171b1d0bb857131cb42f862c
san0di804 <91170798+san0di804@users.noreply.github.com>
2021-12-08 12:04:42 +0800

    Add files via upload

---
+++ week13/week13-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void printstar(int n)
+{
+    for(int i=0;i<n;i++) printf("*");
+}
+void printspace(int n)
+{
+    for(int i=0;i<n;i++) printf(" ");
+}
+int main()
+{
+    for(int i=1;i<10;i++){
+        printspace(9-i);
+        printstar (i);
+        printf("\n");
+    }
+}
