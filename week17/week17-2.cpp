f5df80721304b2da853ff3d93bbb9db72186ebfa
unknown <10160261@me.mcu.edu.tw>
2022-01-05 11:17:57 +0800

    add week17

---
+++ week17/week17-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int n=20030101;
+    while(n>0){
+        printf("%d => %d %d\n",n,n/10,n%10);
+        n=n/10;
+    }
+}
---
+++ week17/week17-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n=11;
+    int sum=0;
+    while(n>0){
+        sum+=n%10;
+        n=n/10;
+    }
+    printf("%d",sum);
+}
